d5f6acf1f5fc2644cca0f1a917e8bc6bd0051682
Gérald Pereira <=>
2015-08-26 23:29:16 +0200

    bootstrap 4

---
+++ GruntFile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // To download bower dependencies in bower_components folder
+        'bower-install-simple': {
+            options: {
+                color: false,
+                production: false,
+                directory: 'bower_components'
+            }
+        },
+        // To includes bower dependencies in  _includes/head.html file
+        wiredep:{
+            dev : {
+                src : [ '_includes/head.html', '_includes/foot.html'],
+                ignorePath : /\.\./,
+                // ignorePath : /\.\.\//, would write dependencies without the leading /
+                exclude: []
+            }
+        },
+        // To include js/*.js in _includes/head.html file
+        includeSource: {
+            options: {
+                //basePath: 'app',
+                //baseUrl: 'app/'
+            },
+            dev: {
+                files: {
+                    '_includes/head.html': '_includes/head.html',
+                    '_includes/foot.html': '_includes/foot.html'
+                }
+            },
+            dist: {
+                files: {
+                    'dist/index.html': 'index.tpl.html'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-install-simple');
+    grunt.loadNpmTasks('grunt-include-source');
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    // Default task(s).
+    grunt.registerTask('build:dev', ['bower-install-simple', 'includeSource:dev', 'wiredep:dev', ]);
+};
